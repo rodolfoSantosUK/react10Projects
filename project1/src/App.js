599dd4ad84c605e7363784b3af8eb9a5ea32f601
Rodolfo Santos <santoseuro2018@gmail.com>
2020-07-03 18:14:47 -0300

    [ADD] Versão inicial do projeto

---
+++ project1/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
+class header extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to react</h2>
+        </header>
+
+      </div>
+    )
+  }
+}
+
+class Intro extends Component {
+  render() {
+    return (
+      <div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reaload
+      </p>
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
